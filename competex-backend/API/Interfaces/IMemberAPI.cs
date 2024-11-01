91a640aa95be55d4237665b048be811928e0455e
Thomas Ilum Andersen <Thomas.ilum.andersen@gmail.com>
2024-11-01 14:26:23 +0100

    Fixed controller failing on POST action Fixed IDs not being added properly for POST action

--- competex-backend/API/Interfaces/IMemberAPI.cs
+++ competex-backend/API/Interfaces/IMemberAPI.cs
@@ -5,6 +5,7 @@ namespace competex_backend.API.Interfaces
 {
     public interface IMemberAPI : IGenericAPI<MemberDTO>
     {
+        // TODO: Change to Task<IActionResult> Or change IGenericAPI to return IActionResult
         IActionResult GetNumber();
 
         IActionResult GetByName(string firstName);
